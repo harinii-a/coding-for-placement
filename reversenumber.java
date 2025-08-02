5c80c7ae171ae6d064bd908b73ab0afe30e18fc7
Harini <harini.vaidy16@gmail.com>
2025-08-02 20:36:11 +0530

    initial commit

---
+++ reversenumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class reversenumber{
+    public static void main(String args[]){
+        int num = 9847839;
+        String res="";
+        while(num>0){
+            int a = num%10;
+            res=res + String.valueOf(a);
+            num/=10;
+        }
+        System.out.println(res);
+    }
+}
